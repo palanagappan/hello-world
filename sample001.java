5cf966660f82ff1ae83cb0233a4bf455ab3d2ff2
palanagappan <palanagappan@gmail.com>
2016-08-03 18:41:18 +0530

    Create sample001.java

---
+++ sample001.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class sample001 {
+
+public static void main(String args[]) {
+	sample1();
+}
+
+public static void sample1() {
+	int ii;
+	int jj = 1;
+	for (ii=1;ii<=1000;ii++)
+		for (jj=1;jj<=1000;jj++)
+			System.out.println("Hello sample001 : "+ii+" : "+jj);
+}
+
+
+}
